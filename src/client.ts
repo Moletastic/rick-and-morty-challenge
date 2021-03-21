40fdf478f5f95034e0acf40c4c0a0d05ce8d6537
jake <boxjack.romero@gmail.com>
2021-03-21 17:39:49 -0300

    fix: Adding pager support using EntityClient

--- src/client.ts
+++ src/client.ts
@@ -1,28 +1,69 @@
 import {AxiosInstance} from "axios"
 import {ICharacter, IEpisode, ILocation, ListResponse} from "./models"
-import {IRAMClient} from "./types"
+import {IEntityClient, IRAMClient} from "./types"
+import {flatSingle} from "./utils"
+
+export class EntityClient<T> implements IEntityClient<T> {
+    constructor(public entity: string, public endpoint: string, public client: AxiosInstance) {}
+
+    async get() {
+        const req = await this.client.get<ListResponse<T>>(this.endpoint)
+        return req.data
+    }
+
+    async getAll() {
+        const {info, results} = await this.get()
+        if (info.pages > 1) {
+            let page = 2
+            const requests: Promise<ListResponse<T>>[] = []
+            while (page <= info.pages) {
+                requests.push(this.getByPage(page++))
+            }
+            const resultset = await Promise.all(requests)
+            const flatted = flatSingle(resultset.map(r => r.results))
+            results.push(...flatted)
+        }
+        return results
+    }
+
+    async getByPage(page: number) {
+        const req = await this.client.get<ListResponse<T>>(`${this.endpoint}/?page=${page}`)
+        return req.data
+    }
+
+    async fetchSome(ids: number[]) {
+        const req = await this.client.get<T[]>(`${this.endpoint}/${ids}`)
+        return req.data
+    }
+
+    async fetch(id: number) {
+        const req = await this.client.get<T>(`${this.endpoint}/${id}`)
+        return req.data
+    }
+}
 
 export default class RAMClient implements IRAMClient {
     client: AxiosInstance
+    character: EntityClient<ICharacter>
+    episode: EntityClient<IEpisode>
+    location: EntityClient<ILocation>
 
     constructor(client: AxiosInstance) {
         this.client = client
-    }
-
-    async getList<T>(endpoint: string): Promise<T[]> {
-        const req = await this.client.get<ListResponse<T>>(endpoint)
-        return req.data.results
+        this.character = new EntityClient("character", "/character", this.client)
+        this.episode = new EntityClient("episode", "/episode", this.client)
+        this.location = new EntityClient("location", "/location", this.client)
     }
 
     async getCharacters() {
-        return this.getList<ICharacter>("/character")
+        return await this.character.getAll()
     }
 
     async getEpisodes() {
-        return this.getList<IEpisode>("/episode")
+        return await this.episode.getAll()
     }
 
     async getLocations() {
-        return this.getList<ILocation>("/location")
+        return await this.location.getAll()
     }
 }
--- src/types/client.ts
+++ src/types/client.ts
@@ -1,8 +1,19 @@
-import {ICharacter, IEpisode, ILocation} from "~/models";
+import {AxiosInstance} from "axios"
+import {ICharacter, IEpisode, ILocation, ListResponse} from "~/models"
+
+export interface IEntityClient<T> {
+    entity: string
+    endpoint: string
+    client: AxiosInstance
+    getAll(): Promise<T[]>
+    getByPage(page: number): Promise<ListResponse<T>>
+    get(): Promise<ListResponse<T>>
+    fetchSome(ids: number[]): Promise<T[]>
+    fetch(id: number): Promise<T>
+}
 
 export interface IRAMClient {
     getCharacters(): Promise<ICharacter[]>
     getEpisodes(): Promise<IEpisode[]>
     getLocations(): Promise<ILocation[]>
 }
-
